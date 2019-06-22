8b5fc030dd125b4071e02052a401d07860966412
MalihaKabir <malihakabir01@gmail.com>
2019-06-22 10:53:41 +0600

    editing scroll effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const scrollAppearFunc = () => {
+/* Scroll for vision */
+const scrollAppearVision = () => {
 	let scroll = document.querySelector('.scroll');
 	let scrollPosition = scroll.getBoundingClientRect().top;
 	let screenPosition = window.innerHeight;
@@ -7,8 +8,9 @@ const scrollAppearFunc = () => {
 		scroll.classList.add('scrollAppear');
 	}
 };
-window.addEventListener('scroll', scrollAppearFunc);
+window.addEventListener('scroll', scrollAppearVision);
 
+/* Scroll for service */
 const scrollAppearFunc1 = () => {
 	let scroll = document.querySelector('.scroll1');
 	let scrollPosition = scroll.getBoundingClientRect().top;
@@ -20,6 +22,7 @@ const scrollAppearFunc1 = () => {
 };
 window.addEventListener('scroll', scrollAppearFunc1);
 
+/* Scroll for requirements */
 const scrollAppearFunc2 = () => {
 	let scroll = document.querySelector('.scroll2');
 	let scrollPosition = scroll.getBoundingClientRect().top;
